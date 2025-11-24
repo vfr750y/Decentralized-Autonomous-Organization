dd6bd0f6e96ed1fa699230f69c1890d57aad444d
vfr750y <th91843@proton.me>
2025-11-25 08:52:29 +1300

    edit test

--- test/MyGovernorTest.t.sol
+++ test/MyGovernorTest.t.sol
@@ -13,5 +13,19 @@ contract MyGovernorTest is Test {
     TimeLock timeLock;
     GovToken govToken;
 
-    function setUp() public {}
+    address public USER = makeAddr("user");
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
+    address[] proposers;
+    address[] executors;
+    uint256 public constant MIN_DELAY = 3600;
+
+    function setUp() public {
+        govToken = new GovToken();
+        govToken.mint(USER, INITIAL_SUPPLY);
+
+        vm.startPrank(USER);
+        govToken.delegate(USER);
+        timeLock = new TimeLock(MIN_DELAY, proposers, executors);
+        governor = new MyGovernor(govToken, timeLock);
+    }
 }
