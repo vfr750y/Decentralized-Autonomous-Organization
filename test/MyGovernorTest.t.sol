02032a2035ace56088029d0202092c357e4ee181
vfr750y <th91843@proton.me>
2025-11-24 21:24:01 +1300

    edit test

--- test/MyGovernorTest.t.sol
+++ test/MyGovernorTest.t.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
+import {MyGovernor} from "../src/MyGovernor.sol";
+import {Box} from "../src/Box.sol";
+import {TimeLock} from "../src/TimeLock.sol";
+import {GovToken} from "../src/GovToken.sol";
 
 contract MyGovernorTest is Test {
     function setUp() public {}
