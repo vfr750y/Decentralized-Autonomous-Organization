b84d391477c85004084d042cbfc12d0b56d235c7
vfr750y <th91843@proton.me>
2025-11-24 20:53:29 +1300

    add Box.sol and edit foundry.toml

---
+++ src/Box.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private s_number;
+    event NumberChanged(uint256 number);
+
+    function store(uint256 newNumber) public onlyOwner {
+        s_number = newNumber;
+        emit NumberChanged(newNumber);
+    }
+
+    function getNumber() public view returns (uint256) {
+        return s_number;
+    }
+}
