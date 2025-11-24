37d0133ec12044f24aeb6c3b322cdeca5d199897
vfr750y <th91843@proton.me>
2025-11-24 21:18:10 +1300

    edit timelockcontroller.sol

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Solidity project for a Decentralized Autonomous Organisation
 A company or organization governed exclusively by code.
 Uses an ERC20 token for governance.
 Proposals are sent to the governor contract
+If approved they are able to be executed but only after a delay which is enforced by the Timelock controller.
--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -2,4 +2,8 @@
 pragma solidity ^0.8.18;
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
-contract TimeLock is TimelockController {}
+contract TimeLock is TimelockController {
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors)
+        TimelockController(minDelay, proposers, executors, msg.sender)
+    {}
+}
